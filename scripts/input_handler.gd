4c7c5643b809565745d20d281c186a685f298a8d
Petr <petr.vasilyev.1996@ya.ru>
2025-12-23 23:41:33 +0300

    await works correctly

---
+++ scripts/input_handler.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+class_name InputHandler
+
+@onready var unit: Unit = $"../Units/Unit"
+
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var mouse_pos = get_global_mouse_position()  # World coordinates
+		var grid_tile = Functions.position_to_grid_coords(mouse_pos)  # Your existing function
+		print("Clicked tile: ", grid_tile)
+		unit.move_to_grid.call_deferred(grid_tile)
--- scripts/unit.gd
+++ scripts/unit.gd
@@ -2,21 +2,45 @@ extends CharacterBody2D
 class_name Unit
 
 var grid_coords: Vector2i;
+var move_tween: Tween;
+
+@onready var visuals: Visuals = $Visuals
+
+const speed: float = 200
+
 
 signal changed_grid_coords(from: Vector2i, to: Vector2i)
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	snap_to_tile()
 
 
-func _on_changed_grid_coords(_from: Vector2, to: Vector2) -> void:
-	position = Functions.grid_coords_to_position(to)
-
-
 func snap_to_tile():
-	print(position)
 	grid_coords = Functions.position_to_grid_coords(position)
 	position = Functions.grid_coords_to_position(grid_coords)
-	print(grid_coords)
+
+	
+func move_to_grid(to: Vector2i):
+	changed_grid_coords.emit(grid_coords, to)
+	var new_position = Functions.grid_coords_to_position(to)
+	run_to(new_position)
 	
 	
+func run_to(target_position: Vector2) -> void:
+	# Cancel any previous tween on this node if needed
+	var move_vector = position - target_position
+	var duration := move_vector.length() / speed
+	update_move_tween(target_position, duration)
+	sprite.flip_h = move_vector.x > 0
+	sprite.play("run")
+	await move_tween.finished
+	sprite.play("idle")
+	
+func update_move_tween(target_position: Vector2, duration: float):
+	if move_tween and move_tween.is_running():
+		move_tween.finished.emit()
+		move_tween.kill()
+	move_tween = create_tween()
+	move_tween.set_trans(Tween.TRANS_LINEAR)
+	move_tween.tween_property(self, "position", target_position, duration)
